b51380fcbd15cc823b2da4ddf783bf4d735da30b
CatalinVoineag <11318084+CatalinVoineag@users.noreply.github.com>
2024-09-28 11:52:41 +0100

    allow deletion of files

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"io"
 	"os"
 	"path/filepath"
@@ -16,6 +17,7 @@ type config struct {
   // lsit files
   list bool
   del bool
+  wLog io.Writer
 }
 
 func main() {
@@ -27,13 +29,29 @@ func main() {
   // Filter options
   ext := flag.String("ext", "", "File extension to filter out")
   size := flag.Int64("size", 0, "Minimum file size")
+  logFile := flag.String("log", "", "Log deletes to this file")
   flag.Parse()
+
+  var (
+    f = os.Stdout
+    err error
+  )
+
+  if *logFile != "" {
+    f, err = os.OpenFile(*logFile, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+    if err != nil {
+      fmt.Fprintln(os.Stderr, err)
+      os.Exit(1)
+    }
+    defer f.Close()
+  }
 
   c := config{
     ext: *ext,
     size: *size,
     list: *list,
     del: *del,
+    wLog: f,
   }
 
   if err := run(*root, os.Stdout, c); err != nil {
@@ -43,7 +61,7 @@ func main() {
 }
 
 func run(root string, out io.Writer, cfg config) error {
-
+  delLogger := log.New(cfg.wLog, "DELETED FILE: ", log.LstdFlags)
   return filepath.Walk(root,
   func(path string, info os.FileInfo, err error) error {
     if err != nil {
@@ -60,7 +78,7 @@ func run(root string, out io.Writer, cfg config) error {
     }
 
     if cfg.del {
-      return delFile(path)
+      return delFile(path, delLogger)
     }
 
     // List is the default option if nothing else was set
